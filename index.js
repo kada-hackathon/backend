ccf950de4fa8464ad119996e5b9ad7af50e4961b
arrizal <arrizalbramadhan09@gmail.com>
2025-10-31 13:59:59 +0700

    fix: correct case-sensitive file import for Linux deployment

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // server.js atau app.js
 require('dotenv').config(); //harus paling atassss
-const dotenv = require('dotenv');
 const connectDB = require('./src/config/db');
 
 // connect to database
@@ -10,5 +9,5 @@ connectDB();
 const app = require('./app');
 
 // Jalankan Server
-const PORT = 5000 ||process.env.PORT; 
+const PORT = process.env.PORT || 5000; 
 app.listen(PORT, console.log(`Server berjalan di mode ${process.env.NODE_ENV} di port ${PORT}`));
